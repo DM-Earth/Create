c32a0d2c3ac7c157a9ecec0ad091464f74597c39
TexTrue <3140846162@qq.com>
2024-07-17 18:10:21 +0800

    init

--- src/main/java/com/simibubi/create/foundation/recipe/AllIngredients.java
+++
@@ -1,9 +0,0 @@
-package com.simibubi.create.foundation.recipe;
-
-import net.fabricmc.fabric.api.recipe.v1.ingredient.CustomIngredientSerializer;
-
-public class AllIngredients {
-	public static void register() {
-		CustomIngredientSerializer.register(BlockTagIngredient.Serializer.INSTANCE);
-	}
-}
--- src/main/java/com/simibubi/create/foundation/utility/fabric/AbstractMinecartExtensions.java
+++
@@ -1,9 +0,0 @@
-package com.simibubi.create.foundation.utility.fabric;
-
-import com.simibubi.create.content.contraptions.minecart.capability.MinecartController;
-
-public interface AbstractMinecartExtensions {
-	MinecartController create$getController();
-
-	String CAP_KEY = "Controller";
-}
